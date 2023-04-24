e7ce673fe80c5905cc77732ffddbaf6994dae02e
Leonardo <lmedeiros045@gmail.com>
2023-04-24 12:39:57 -0300

    Criado applicativo angular - pequena lista  todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
+
+import { TodoComponentComponent } from './todo-component/todo-component.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo_Module';
+  @ViewChild("todoContainer", {read: ViewContainerRef }) vcr!: ViewContainerRef;
+  ref!: ComponentRef<TodoComponentComponent>
+
+  input_text: string | undefined;
+
+
+  construir_todo() {
+    if (this.input_text == null){
+      alert("Sem texto para adicionar...");
+    } else{
+      this.ref = this.vcr.createComponent(TodoComponentComponent)
+      this.ref.instance.input_text = this.input_text;
+
+    }
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponentComponent } from './todo-component/todo-component.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponentComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
